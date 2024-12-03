f6339a1c57ce519a6a093abcf9a6da4f5d7f094b
jarlini <jalujalu998@gmail.com>
2024-12-03 16:22:14 +0530

    first commilliiiliit111

---
+++ src/component/Api.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+
+// Retrieve token from localStorage
+const token = localStorage.getItem('token');
+
+// Create an Axios instance with a base URL and Authorization header
+const api = axios.create({
+  baseURL: `${process.env.URL}/api/admin`,  // Change this to your actual API base URL
+  headers: {
+    Authorization: `Bearer ${token}`, // Add token to headers
+  },
+});
+
+// Export the Axios instance to use in other parts of your app
+export default api;
